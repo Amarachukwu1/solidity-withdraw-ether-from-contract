4dab9d597f0fb6bb7784e37831f85f46717aba8e
Amarachukwu1 <97677585+Amarachukwu1@users.noreply.github.com>
2022-02-06 01:57:16 +0100

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,14 @@
 require('dotenv').config() ;
 
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-const mnemonic = process.env.MNENOMIC;
+const mnemonic = process.env.MNEMONIC;
 
 module.exports = {
   networks: {
     mainnet: {
      provider: () => new HDWalletProvider(bridge isolate pitch hire chunk woman copper before subject pause bridge glare, `https://cloudflare-eth.com`),
      network_id: 1,
-     gas: 320000,
+     gas: 3200000,
     }
   },
 };
