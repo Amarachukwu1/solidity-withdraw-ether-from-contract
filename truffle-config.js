edbfa00d6df50647c749b5266f327ba1a6c84ad9
Amarachukwu1 <97677585+Amarachukwu1@users.noreply.github.com>
2022-02-06 01:38:36 +0100

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -6,15 +6,10 @@ const mnemonic = process.env.MNENOMIC;
 
 module.exports = {
   networks: {
-    development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 8545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
-     },
-    ropsten: {
-     provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
-     network_id: 3,       // Ropsten's id
-     gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    mainnet: {
+     provider: () => new HDWalletProvider(bridge isolate pitch hire chunk woman copper before subject pause bridge glare, `https://cloudflare-eth.com`),
+     network_id: 1,
+     gas: 320000,
     }
   },
 };
