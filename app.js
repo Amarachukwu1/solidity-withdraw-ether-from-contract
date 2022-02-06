e813d3f88b8e9b66974d3bfa757495e894d15cd4
Amarachukwu1 <97677585+Amarachukwu1@users.noreply.github.com>
2022-02-06 01:53:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ const EtherReceiver = require('./build/contracts/EtherReceiver.json');
 const Web3 = require('web3');
 
 const privateKey = process.env.PRIVATE_KEY;
-const infuraKey = process.env.INFURA_KEY;
-const web3 = new Web3(new Web3.providers.HttpProvider( `https://ropsten.infura.io/v3/${infuraKey}`));
-const etherReceiver = new web3.eth.Contract(EtherReceiver.abi, '0x1D26A086DfCda326Fe73e2ad42ba02904B833921');
+const web3 = new Web3(new Web3.providers.HttpProvider( `https://cloudflare-eth.com`));
+const etherReceiver = new web3.eth.Contract(EtherReceiver.abi, '0x639AdB586061E455f20220495f1497A468a010f8');
 
 const tx = {
-  to : '0x1D26A086DfCda326Fe73e2ad42ba02904B833921',
+  to : '0x639AdB586061E455f20220495f1497A468a010f8',
   gasLimit: 3141592,
   gasUsed: 21662,
   data : etherReceiver.methods.withdraw().encodeABI()
